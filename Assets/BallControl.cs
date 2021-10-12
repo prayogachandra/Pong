24b32ade5af09f35d6c3b16f90e7ceeacf861e98
prayogachandra <63632592+prayogachandra@users.noreply.github.com>
2021-10-12 11:03:24 +0700

    Add files via upload

---
+++ Assets/BallControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+	//Rigidbody 2D Bola
+	private Rigidbody2D rigidBody2D;
+
+	//besarnya gaya awal yang diberikan untuk mendorong bola
+	public float xInitialForce;
+	public float yInitialForce;
+
+	private Vector2 trajectoryOrigin;
+	void ResetBall(){
+		//Reset posisi menjadi (0.0)
+		transform.position = Vector2.zero;
+
+		//Reset kecepatan menjadi (0.0)
+		rigidBody2D.velocity = Vector2.zero;
+	}
+
+	void PushBall(){
+		float yRandomInitialForce = Random.Range(-yInitialForce,yInitialForce);
+		float randomDirection = Random.Range(0,2);
+		if (randomDirection < 1.0f) {
+			rigidBody2D.AddForce(new Vector2(-xInitialForce,yRandomInitialForce));
+		} else {
+			rigidBody2D.AddForce(new Vector2(xInitialForce,yRandomInitialForce));
+		}
+	}
+
+	void RestartGame(){
+		ResetBall();
+		Invoke("PushBall", 2);
+	}
+
+	// Use this for initialization
+	void Start () {
+		rigidBody2D = GetComponent<Rigidbody2D>();
+		RestartGame();
+		trajectoryOrigin = transform.position;
+	}
+
+	private void OnCollisionExit2D(Collision2D collision){
+		trajectoryOrigin = transform.position;
+	}
+
+	public Vector2 TrajectoryOrigin{
+		get{ return trajectoryOrigin;}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SideWall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour {
+	public PlayerControl player;
+	[SerializeField]
+	public GameManager gameManager;
+	void OnTriggerEnter2D(Collider2D anotherCollider){
+		if(anotherCollider.name == "ball"){
+			player.IncrementScore ();
+			if(player.Score < gameManager.maxScore){
+				anotherCollider.gameObject.SendMessage ("RestartGame",2.0f,SendMessageOptions.RequireReceiver);
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
